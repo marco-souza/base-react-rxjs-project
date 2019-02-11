1b466f42ee32362da49728ab180d2b5749b5c81d
Marco Antônio <ma.souza.junior@gmail.com>
2019-02-10 22:38:25 -0200

    FEA : use store state

--- src/app/screens/home/index.js
+++ src/app/screens/home/index.js
@@ -1,31 +1,51 @@
+// import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
+import Input from '@material-ui/core/Input'
 import AccessAlarmIcon from '@material-ui/icons/AccessAlarm'
+
+import useUser from 'app/ducks/user'
 
 
 // Define Component
-const Component = ({ name }) => (
-  <div>
-    <Grid container spacing={24}>
-      <Grid item xs={12}>
-        <Paper>
-          <Typography>
-            Hello {name}
-            <AccessAlarmIcon />
-          </Typography>
-        </Paper>
+const Component = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <Grid container spacing={24}>
+        <Grid item xs={12}>
+          <Paper>
+            <Typography>
+              Name {props.name} <br/>
+              Photo: {props.photo}
+              <AccessAlarmIcon /><br/>
+
+              <Input
+                onChange={event => props.changeName(event.target.value)}
+              />
+              <Input
+                onChange={event => props.changePhoto(event.target.value)}
+              />
+            </Typography>
+          </Paper>
+
+
+        </Grid>
       </Grid>
-    </Grid>
-  </div>
-)
+    </div>
+  )
+}
 
 
 // Define Props Types
 Component.propTypes = {
-  name: PropTypes.string.isRequired,
+  name: PropTypes.string,
+  photo: PropTypes.string,
+  changeName: PropTypes.func.isRequired,
+  changePhoto: PropTypes.func.isRequired,
 }
 
 
-export default Component
+export default useUser(Component)
