bce74c6cdde3849353b97ec7400af06c00704991
Marco Antônio <ma.souza.junior@gmail.com>
2019-02-17 22:56:28 -0300

    ENH : split component composition

--- src/app/screens/home/index.js
+++ src/app/screens/home/index.js
@@ -2,8 +2,9 @@ import Component from './component'
 import userData from 'app/ducks/user'
 
 
+let ComposedComponent = Component
 // Apply decrators to inject data and functions to component props
-let ComposedComponent = userData(Component)
+ComposedComponent = userData(ComposedComponent)
 
 
 // Export composed component
