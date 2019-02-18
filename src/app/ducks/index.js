728d1875215308fa23b892a65b8e7bd0569194f1
Marco Antônio <ma.souza.junior@gmail.com>
2019-02-17 22:57:37 -0300

    FEA : add apic middleware

--- src/app/ducks/index.js
+++ src/app/ducks/index.js
@@ -1,32 +1,38 @@
-/**
- * This file should setup an the redux store, so each new duck should be added
- * to **initialState** and **reducers** to allow it's actions to change state.
- */
-import { createStore, combineReducers, compose } from 'redux'
+// #region Imports
+import {
+  createStore,
+  compose,
+  applyMiddleware,
+} from 'redux'
+import { createEpicMiddleware } from 'redux-observable'
 
-// Import ducks definitions
-import * as user from './user'
-
-
-// Setup Store initial state
-const initialState = {
-  user: user.INITIAL_STATE,
-}
-
-
-// Compine all ducks reducers - (receive state + action, return new state)
-const reducers = combineReducers({
-  user: user.reducer,
-})
-
-
-// Create store
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-export const STORE = createStore(
-  // Add reducers
-  reducers,
-  // Set initial state
+// Import roots
+import {
   initialState,
-  // Apply enhancements
-  composeEnhancers(),
-)
+  rootEpic,
+  rootReducer,
+} from './root'
+// #endregion
+
+
+// #region Create store
+export const configureStore = () => {
+  const epicMiddleware = createEpicMiddleware()
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  const store = createStore(
+    // Add reducers
+    rootReducer,
+    // Set initial state
+    initialState,
+    // Apply enhancements
+    composeEnhancers(
+      applyMiddleware(epicMiddleware),
+    ),
+  )
+
+  // Run
+  epicMiddleware.run(rootEpic)
+
+  return store
+}
+// #endregion
