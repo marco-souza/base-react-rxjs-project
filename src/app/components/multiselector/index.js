53d6080c5e0b5f72d375be865b9c4738c0f8adea
Marco Antônio <ma.souza.junior@gmail.com>
2019-03-10 18:46:41 -0300

    ENH : pass state to selector

--- src/app/components/multiselector/index.js
+++ src/app/components/multiselector/index.js
@@ -2,11 +2,13 @@ import Component from './component'
 import styles from './styles'
 
 import useStyles from 'react-jss'
+import userSearchBox from 'app/ducks/searchBox'
 
 
 let ComposedComponent = Component
 // Apply decrators to inject data and functions to component props
 ComposedComponent = useStyles(styles)(ComposedComponent)
+ComposedComponent = userSearchBox(ComposedComponent)
 
 
 // Export composed component
