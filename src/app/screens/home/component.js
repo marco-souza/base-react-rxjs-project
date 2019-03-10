b2a208f9ae1036605141cabc0216c1665cf4cbca
Marco Antônio <ma.souza.junior@gmail.com>
2019-03-10 16:29:57 -0300

    ENH : add search to home screen

--- src/app/screens/home/component.js
+++ src/app/screens/home/component.js
@@ -1,44 +1,12 @@
 import { Fragment } from 'react'
 import PropTypes from 'prop-types'
-import Grid from '@material-ui/core/Grid'
-import Paper from '@material-ui/core/Paper'
-import Typography from '@material-ui/core/Typography'
-import Input from '@material-ui/core/Input'
-import Button from '@material-ui/core/Button'
-import AccessAlarmIcon from '@material-ui/icons/AccessAlarm'
+
+import Search from 'app/components/search'
 
 
 const Component = (props) => (
   <Fragment>
-    <Grid container spacing={16}>
-
-      <Grid item xs={12}>
-        <Paper>
-          <Typography>
-            Name {props.name} <br/>
-            <AccessAlarmIcon /><br/>
-          </Typography>
-
-          <div>
-            <Input
-              value={props.name}
-              onChange={event => props.changeName(event.target.value)}
-            />
-          </div>
-          <Button
-            onClick={() => props.fetchProfile(props.name)}
-          >
-            search
-          </Button>
-
-          <Typography>
-            {JSON.stringify(props.profile, null, 4)}
-          </Typography>
-
-        </Paper>
-      </Grid>
-
-    </Grid>
+    <Search />
   </Fragment>
 )
 
