88361cbd5aea0cb8fb3e58e4241abb1107f7ab9f
Marco Antônio <ma.souza.junior@gmail.com>
2019-03-10 16:23:09 -0300

    FEA : add search component

---
+++ src/app/components/search/component.js
@@ -0,0 +1,21 @@
+import { Fragment } from 'react'
+import PropTypes from 'prop-types'
+
+
+const Component = ({
+  classes,
+}) => (
+  <Fragment>
+
+  </Fragment>
+)
+
+
+// Define Props Types
+Component.propTypes = {
+  // ducks' data
+  classes: PropTypes.object,
+}
+
+
+export default Component
